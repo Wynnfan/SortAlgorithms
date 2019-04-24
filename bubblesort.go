fb4adb88de7b2df0d2546cb87027f70e8b5029a3
Wynnfan <Wynnfan1997@gmail.com>
2019-04-24 21:36:16 +0800

    fix bug

--- bubblesort.go
+++ bubblesort.go
@@ -4,30 +4,27 @@ import (
 	"fmt"
 )
 
-var a = make([]int, 101)
+// var a = make([]int, 101)
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	fmt.Println(n)
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &a[i])
-	}
-	BubbleSort(n, a)
+	a := []int{1, 5, 3, 7, 2}
 
-	for i := 0; i < n; i++ {
+	BubbleSort(5, a)
+
+	for i := 0; i < 5; i++ {
 		fmt.Printf("%d ", a[i])
 	}
 }
 
 // 冒泡过程
 func BubbleSort(n int, a []int) {
-	var i int
-	for i = 0; i < n; i++ {
-		for j := 0; j < n-i; j++ {
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
 			if a[j] < a[j+1] {
 				a[j], a[j+1] = a[j+1], a[j]
 			}
 		}
 	}
+
 }
